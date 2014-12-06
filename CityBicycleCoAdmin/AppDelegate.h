2c20133ca32e67de219944be671c6a3157fba79c
Archer <archer@oodalalee.com>
2014-12-06 12:33:31 -0800

    Initial Commit

---
+++ CityBicycleCoAdmin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CityBicycleCoAdmin
+//
+//  Created by Supreme Overlord on 12/6/14.
+//  Copyright (c) 2014 Supreme Overlord. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CityBicycleCoAdmin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CityBicycleCoAdmin
+//
+//  Created by Supreme Overlord on 12/6/14.
+//  Copyright (c) 2014 Supreme Overlord. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
